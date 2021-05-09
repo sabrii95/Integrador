48c1a8f1c991f26961c3e013dfcfbca6afaec0a1
Sabrina Cabral <slcabral@T0003183142.corp.cablevision.com.ar>
2021-05-08 23:09:15 -0300

    Listar pokemon por color

--- Js/Ajax.js
+++ Js/Ajax.js
@@ -1,46 +1,51 @@
 
 $(document).ready(() => {
 
-    $(buscar).click( ()=>{
-    let input = $(search).val().trim();
-    buscarColor(input);
+    $(buscar).click(() => {
+        let input = $(search).val().trim();
+        buscarColor(input);
 
-    
+
     })
-    
-    const buscarColor = (color)=>{
-        $.ajax({
-            url: `https://pokeapi.co/api/v2/pokemon-color/${color}`,
-            method: "GET",
-            async: true,
-            success: (response) => {
-                        var json =   response.pokemon_species;
-                        
-                        json.map(x=>{
-                            console.log(x.url)
-                        
-                            $("<article>", {
-                                'class': 'pokemon'
-                            }).append(
-                                $('<img>', {
-                                    'src': x.url,
-                                    'alt': x.name              
-                                }),
-                                $('<h5>', {
-                                    'text': x.name                                
-                                })
-                                
-                        
-                            ).hide().appendTo('#listado-pokemon').fadeIn('slow');
 
-                        });
-                    
-                    },
-            error: (xhr) => {
-                console.log(xhr)
+    const buscarColor = async (color) => {
+
+        const response = await $.ajax(`https://pokeapi.co/api/v2/pokemon-color/${color}`);
+        var json = response.pokemon_species;
+
+        json.map(async x => {
+
+            let image = await buscarPokemon(x.name)
+            if (image.code != 400) {
+                $("<article>", {
+                    'class': 'pokemon'
+                }).append(
+                    $('<img>', {
+                        'src': image.response,
+                        'alt': x.name
+                    }),
+                    $('<h5>', {
+                        'text': x.name
+                    })
+                ).hide().appendTo('#listado-pokemon').fadeIn('fast');
             }
         });
+
     }
 
+    const buscarPokemon = async (name) => {
+        try {
+            const response = await $.ajax(`https://pokeapi.co/api/v2/pokemon/${name}`);
+   
+
+            return {code:200, response:`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${response.id}.png`};
+        } catch ({ error }) {
+            return {code:400, response: error}
+
+        }
+
+
+    }
 
 })
+
--- Js/scripts.js
+++ Js/scripts.js
@@ -2,6 +2,7 @@ console.log("hola")
 window.onload = () => {
     const boton = document.getElementById('icono-desp');
     boton.addEventListener('click', ()=>{
+        console.log("presionaste el boton")
         const ul = document.getElementById('elemento');
         ul.classList.toggle('show');
     })
