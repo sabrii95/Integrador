343a1b7a2107d9910acd2dbe76c67b28cfd383ca
beotrera <otrera996@gmail.com>
2021-05-09 20:36:07 -0300

    firts commit

--- Js/Ajax.js
+++ Js/Ajax.js
@@ -4,10 +4,8 @@ $(document).ready(() => {
     $(buscar).click( ()=>{
     let input = $(search).val().trim();
     buscarColor(input);
-
-    
     })
-    
+
     const buscarColor = (color)=>{
         $.ajax({
             url: `https://pokeapi.co/api/v2/pokemon-color/${color}`,
@@ -15,26 +13,21 @@ $(document).ready(() => {
             async: true,
             success: (response) => {
                         var json =   response.pokemon_species;
-                        
                         json.map(x=>{
                             console.log(x.url)
-                        
                             $("<article>", {
                                 'class': 'pokemon'
                             }).append(
                                 $('<img>', {
                                     'src': x.url,
-                                    'alt': x.name              
+                                    'alt': x.name
                                 }),
                                 $('<h5>', {
-                                    'text': x.name                                
+                                    'text': x.name
                                 })
-                                
-                        
                             ).hide().appendTo('#listado-pokemon').fadeIn('slow');
 
                         });
-                    
                     },
             error: (xhr) => {
                 console.log(xhr)
