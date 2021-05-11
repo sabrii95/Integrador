cf744c98b375a20892b9b72cddace409d4c3df1e
beotrera <otrera996@gmail.com>
2021-05-11 14:16:33 -0300

    se agregan modificaciones el html

--- Js/Ajax.js
+++ Js/Ajax.js
@@ -1,39 +1,56 @@
+colors = {rock:"#b8a058",normal:"#a8a090",fighting:"#a05038",flying:"#98a8f0",poison:"#b058a0",ground:"#d0b058",bug:"#a8b820",ghost:"#6060b0",steel:"#a8a8c0"
+,fire:"#f05030",water:"#3b8fe3",grass:"#78c850",electric:"#f8bf30",psychic:"#f870a0",ice:"#58c7e0",dragon:"#7760e0",dark:"#705848",fairy:"#e794e7"}
 
-$(document).ready(() => {
+const  PokemonList = document.getElementById('poke');
+var openM = true;
 
-    $(buscar).click( ()=>{
-    let input = $(search).val().trim();
-    buscarColor(input);
-    })
+const openMenu=()=>{
+        if(openM){
+            document.getElementById('mobile-menu').classList.add('active');
+            openM = !openM;
+        }
+        else{
+            document.getElementById('mobile-menu').classList.remove('active');
+            openM = !openM;
+        }
 
-    const buscarColor = (color)=>{
-        $.ajax({
-            url: `https://pokeapi.co/api/v2/pokemon-color/${color}`,
-            method: "GET",
-            async: true,
-            success: (response) => {
-                        var json =   response.pokemon_species;
-                        json.map(x=>{
-                            console.log(x.url)
-                            $("<article>", {
-                                'class': 'pokemon'
-                            }).append(
-                                $('<img>', {
-                                    'src': x.url,
-                                    'alt': x.name
-                                }),
-                                $('<h5>', {
-                                    'text': x.name
-                                })
-                            ).hide().appendTo('#listado-pokemon').fadeIn('slow');
+}
 
-                        });
-                    },
-            error: (xhr) => {
-                console.log(xhr)
-            }
-        });
+const createPokemon = async()=>{
+    for(let x=1; x <= 151; x++){
+        await getPokemon(x)
     }
+}
 
+const getPokemon = async id=>{
+    const url =`https://pokeapi.co/api/v2/pokemon/${id}/`;
+    var data;
+    await $.ajax({
+        url: url,
+        method: "GET",
+        async: true,
+        success: (res) => {
+            data=res;
+        },
+        error: (xhr) => {console.log(xhr)}
+    });
 
-})
+    createItem(data);
+}
+
+const createItem = (pokemon)=>{
+
+    let name=pokemon.forms[0].name;
+    name=name[0].toUpperCase() + name.slice(1);
+    let img =`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`;
+
+    const Element = document.createElement("li");
+    Element.classList.add("Poke-card")
+    const html=`<figure><img src=${img} alt="" class="poke-img"/><figure><p>${name}</p>`
+
+    Element.innerHTML = html;
+    PokemonList.appendChild(Element)
+
+}
+
+createPokemon()
--- Js/scripts.js
+++
@@ -1,9 +0,0 @@
-console.log("hola")
-window.onload = () => {
-    const boton = document.getElementById('icono-desp');
-    boton.addEventListener('click', ()=>{
-        const ul = document.getElementById('elemento');
-        ul.classList.toggle('show');
-    })
-
-}
